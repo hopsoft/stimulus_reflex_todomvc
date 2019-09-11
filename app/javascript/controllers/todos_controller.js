6b37c61af16507256f9315c6aead1b91003ed7a9
Nathan Hopkins <natehop@gmail.com>
2019-09-10 18:22:32 -0600

    Support cancel edit

---
+++ app/javascript/controllers/todos_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from 'stimulus';
+import StimulusReflex from 'stimulus_reflex';
+
+export default class extends Controller {
+  connect() {
+    console.log('connected....');
+    StimulusReflex.register(this);
+  }
+
+  cancelEdit(event) {
+    if (event.type === 'keypress' && event.code !== 'Escape') return;
+    this.stimulate('TodosReflex#cancel_edit');
+  }
+}
