34ea58b639d22b32a63ea99085d6280005118dda
pjf <pjforde1978@gmail.com>
2019-09-14 19:42:22 -0400

    first pass working

--- app/javascript/controllers/todos_controller.js
+++ app/javascript/controllers/todos_controller.js
@@ -1,15 +1,20 @@
-import { Controller } from 'stimulus';
-import StimulusReflex from 'stimulus_reflex';
+import { Controller } from 'stimulus'
+import StimulusReflex from 'stimulus_reflex'
 
 export default class extends Controller {
-  connect() {
-    StimulusReflex.register(this);
+  connect () {
+    StimulusReflex.register(this)
   }
 
   cancelEdit (event) {
-    if (event.type === 'keydown' && !['Escape', 'Esc'].includes(event.key)) {
+    if (event.type === 'keydown') {
+      if (['Escape', 'Esc'].includes(event.key)) event.target.blur()
       return
     }
-    this.stimulate('TodosReflex#cancel_edit');
+    this.stimulate('TodosReflex#cancel_edit')
+  }
+
+  onEdit () {
+    document.querySelector('[autofocus]').focus()
   }
 }
