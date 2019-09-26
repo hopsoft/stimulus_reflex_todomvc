3644b213b3c12d3c596864017ca83116ee0f3eec
Nathan Hopkins <natehop@gmail.com>
2019-09-25 21:38:45 -0600

    Remove unused helper

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,8 +1,4 @@
 module TodosHelper
-  def header_html
-    "#{tag.small session[:filter]} todos".html_safe
-  end
-
   def filter_css(filter)
     "selected" if session[:filter] == filter.to_s
   end
