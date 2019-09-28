2f6ef93eb44ed24a75d8e5acb234ad347e13db1a
Nate Hopkins <natehop@gmail.com>
2019-09-28 11:10:10 -0600

    Demo showcasing lifecycle methods (#27)

--- app/javascript/controllers/todos_controller.js
+++ app/javascript/controllers/todos_controller.js
@@ -1,15 +1,37 @@
-import { Controller } from 'stimulus';
-import StimulusReflex from 'stimulus_reflex';
+import ApplicationController from './application_controller';
+import Velocity from 'velocity-animate';
 
-export default class extends Controller {
-  connect() {
-    StimulusReflex.register(this);
-  }
+export default class extends ApplicationController {
+  static targets = ['list'];
 
   cancelEdit (event) {
-    if (event.type === 'keydown' && !['Escape', 'Esc'].includes(event.key)) {
-      return
-    }
+    if (event.type === 'keydown' && !['Escape', 'Esc'].includes(event.key)) return;
     this.stimulate('TodosReflex#cancel_edit');
+  }
+
+  afterCreate () {
+    this.highlight(this.lastListItem);
+  }
+
+  afterToggle (checkbox) {
+    this.highlight(checkbox.closest('li'));
+  }
+
+  afterToggleAll () {
+    this.listItems.forEach(li => this.highlight(li));
+  }
+
+  highlight (element) {
+    Velocity(element, { scale: 1.01, backgroundColor: '#ffff99' }, 200).then(
+      Velocity(element, { scale: 1, backgroundColor: '#fff' }, 600)
+    );
+  }
+
+  get listItems () {
+    return this.listTarget.querySelectorAll('li');
+  }
+
+  get lastListItem () {
+    return this.listItems[this.listItems.length - 1];
   }
 }
