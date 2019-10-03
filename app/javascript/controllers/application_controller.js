d2fc00d9ccaf72d38658b50f67cf23a814a5cbd2
pjf <pjforde1978@gmail.com>
2019-10-03 05:23:05 -0400

    simple selectors demo

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,16 +1,24 @@
 import { Controller } from 'stimulus'
 import StimulusReflex from 'stimulus_reflex'
 
+let benchmark
+
 export default class extends Controller {
   connect () {
     StimulusReflex.register(this)
   }
 
   beforeReflex () {
+    benchmark = performance.now()
     document.body.classList.add('wait')
   }
 
-  afterReflex () {
+  afterReflex (element) {
+    console.log(
+      `${element.dataset.reflex.split('#')[1]} took ${(
+        performance.now() - benchmark
+      ).toFixed(0)}ms.`
+    )
     document.body.classList.remove('wait')
 
     const focusElement = this.element.querySelector('[autofocus]')
